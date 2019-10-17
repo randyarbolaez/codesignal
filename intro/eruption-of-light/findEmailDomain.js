042af2d2519166d641b6bc08d1c9c38285b5c649
Randy <randyarbolaez0@gmail.com>
2019-10-17 08:03:29 -0400

    eruption-of-light 1/5

---
+++ intro/eruption-of-light/findEmailDomain.js
@@ -0,0 +1,7 @@
+// An email address such as "John.Smith@example.com" is made up of a local part ("John.Smith"), an "@" symbol, then a domain part ("example.com").
+
+function findEmailDomain(address) {
+  let addressArr = address.split('@');
+
+  return addressArr[addressArr.length - 1];
+}
