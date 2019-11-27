54e8afe36220dadec84f39e7655dd763f18dab67
Randy <randyarbolaez0@gmail.com>
2019-11-27 04:31:37 -0500

    daily-challenge 041

---
+++ daily-challenges/properNounCorrection.js
@@ -0,0 +1,10 @@
+// Proper nouns always begin with a capital letter, followed by small letters.
+
+function properNounCorrection(noun) {
+  noun = noun
+    .toLowerCase()
+    .split("")
+    .join("");
+
+  return noun.replace(noun[0], noun[0].toUpperCase());
+}
