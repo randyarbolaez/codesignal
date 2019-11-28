c8fc3b57cb880dcb4bad3ca0edced9d3d50fcb32
Randy <randyarbolaez0@gmail.com>
2019-11-28 04:51:02 -0500

    daily-challenge 042

---
+++ daily-challenges/willYou.js
@@ -0,0 +1,5 @@
+// Once Mary heard a famous song, and a line from it stuck in her head. That line was "Will you still love me when I'm no longer young and beautiful?". Mary believes that a person is loved if and only if he/she is both young and beautiful, but this is quite a depressing thought, so she wants to put her belief to the test.
+
+function willYou(young, beautiful, loved) {
+  return (young && beautiful) !== loved;
+}
