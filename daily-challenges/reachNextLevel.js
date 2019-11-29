0cae69f665931f49e6efecbb267c305cd417bb92
Randy <randyarbolaez0@gmail.com>
2019-11-29 08:23:49 -0500

    daily-challenge 044

---
+++ daily-challenges/reachNextLevel.js
@@ -0,0 +1,5 @@
+// You are playing an RPG game.Currently your experience points(XP) total is equal to experience.To reach the next level your XP should be at least at threshold.If you kill the monster in front of you, you will gain more experience points in the amount of the reward.
+
+function reachNextLevel(experience, threshold, reward) {
+  return experience + reward >= threshold;
+}
