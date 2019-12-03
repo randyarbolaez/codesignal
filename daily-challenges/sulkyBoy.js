b6213bffa3fbe5daa2d0801468a4538c2526a051
Randy <randyarbolaez0@gmail.com>
2019-12-03 04:27:13 -0500

    daily-challenge 052

---
+++ daily-challenges/sulkyBoy.js
@@ -0,0 +1,5 @@
+// Little boy's mother didn't buy him a candy, so now he's sulky and moody. He turns everything you say upside down! If you say that something is true, he'll say it's false, and vice versa.
+
+function sulkyBoy(statement) {
+  return !statement;
+}
