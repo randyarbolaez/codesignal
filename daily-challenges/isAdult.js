8c37685e6e4ec218ca47b8b3daaf0d883112c95b
Randy <randyarbolaez0@gmail.com>
2019-12-18 04:33:35 -0500

    daily-challenge 82

---
+++ daily-challenges/isAdult.js
@@ -0,0 +1,5 @@
+// Given the age of a person and the adulthood age in a particular country, determine if the person is considered an adult in that country. Note that people whose age is the same as the adulthood age are considered adult.
+
+function isAdult(age, adulthoodAge) {
+  return age >= adulthoodAge;
+}
