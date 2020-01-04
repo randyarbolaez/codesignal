441900eda5275df6d2154a5f5392b9385db8333d
Randy <randyarbolaez0@gmail.com>
2020-01-04 18:08:36 -0500

    daily-challenge 105

---
+++ daily-challenges/arithmeticExpression.js
@@ -0,0 +1,9 @@
+// Consider an arithmetic expression of the form a#b=c. Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
+
+function arithmeticExpression(a, b, c) {
+  if (c == a + b) return true;
+  if (c == a - b) return true;
+  if (c == a * b) return true;
+  if (c == a / b) return true;
+  return false;
+}
