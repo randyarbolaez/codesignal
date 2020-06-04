52a1da2978c2b73df17aa38b6ee9e5e6e4956aa7
Randy <randyarbolaez0@gmail.com>
2020-06-04 04:37:13 -0400

    daily-challenge 152

---
+++ daily-challenges/isPerfectScore.py
@@ -0,0 +1,5 @@
+# You are asked to implement a grader that checks whether a 3 question test score is perfect based on the correctness of answers to each of the 3 questions.
+
+def isPerfectScore(ans1, ans2, ans3):
+    return ans1 and ans2 and ans3
+
