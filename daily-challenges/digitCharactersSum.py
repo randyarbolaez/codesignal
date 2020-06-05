92dc78bdd33074116bf1e5ee5d7e46c4f866758a
Randy <randyarbolaez0@gmail.com>
2020-06-05 06:57:15 -0400

    daily-challenge 153

---
+++ daily-challenges/digitCharactersSum.py
@@ -0,0 +1,5 @@
+# Given two chars each representing a one-digit number, return a string which represents the sum of these numbers.
+
+def digitCharactersSum(ch1, ch2):
+    return str(int(ch1) + int(ch2))
+
